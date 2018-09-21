d6ddbbb1270d0c7117e484f316d262fa82215418
Serge Fonville <serge.fonville@gmail.com>
2018-09-21 12:05:07 +0200

    Templates

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Serge Fonville',
+        'title': 'Blog Post 1',
+        'content': 'First blog post',
+        'date_posted': 'September 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second blog post',
+        'date_posted': 'September 21, 2018'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
